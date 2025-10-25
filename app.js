575e7b62ae87d8ba71a9fad95d7d5f9f1d205bf8
Tran Minh Tri <tmtri310251@gmail.com>
2025-10-25 23:17:56 +0700

    test collapse attribution and slider leap

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ const map = new maplibregl.Map({
     [105.5, 20.7],
     [106.1, 21.5],
   ],
-  attributionControl: false,
+  attributionControl: { compact: true },
 });
 // --- 2. MAP LOAD ---
 map.on("load", () => {
